9973d6e3fea6eaf4cba1a0807585553a44afef6c
zhirobyte <faizhanafi90@gmail.com>
2022-12-28 22:01:52 +0700

    add import asyncio to faster progress

--- cek-bulk.py
+++ cek-bulk.py
@@ -1,21 +1,36 @@
-import requests
+import asyncio
+import aiohttp
 
-# Open the file containing the list of URLs
-with open('url_list.txt', 'r') as f:
-    # Read each line in the file (each line is a URL)
-    for url in f:
-        # Strip leading/trailing whitespace and newline characters from the URL
-        url = url.strip()
+# Define the ANSI escape code for green text
+GREEN = '\033[92m'
+
+# Reset the text color to the default
+RESET = '\033[0m'
+
+async def check_url(url):
+    # Make a GET request to the URL
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            # Check if the response is a valid one (2xx status code) and if the phrase "page not found" does not appear in the response content
+            if response.status == 200 and "page not found" not in await response.text():
+                # Print the URL in green text
+                print(f'{GREEN}{url}{RESET} is a valid URL')
+
+async def main():
+    # Open the file containing the list of URLs
+    with open('stn.txt', 'r') as f:
+        # Read each line in the file (each line is a URL)
+        tasks = []
+        for url in f:
+            # Strip leading/trailing whitespace and newline characters from the URL
+            url = url.strip()
+            
+            # Create a task to check the URL asynchronously
+            task = asyncio.create_task(check_url(url))
+            tasks.append(task)
         
-        # Make a GET request to the URL
-        response = requests.get(url)
-        
-        # Check if the response is a valid one (2xx status code)
-        if response.status_code // 100 == 2:
-            # Check if the phrase "page not found" appears in the response content
-            if "page not found" in response.text.lower():
-                print(f'{url} is NOT a valid URL')
-            else:
-                print(f'{url} is a valid URL')
-        else:
-            print(f'{url} is NOT a valid URL')
+        # Wait for all tasks to complete
+        await asyncio.gather(*tasks)
+
+# Run the main function
+asyncio.run(main())
