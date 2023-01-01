f2351fc2a8b7c93a11143e700cad11db84a84699
zhirobyte <faizhanafi90@gmail.com>
2023-01-02 01:05:21 +0700

    update bug

--- Scrapjs.py
+++ Scrapjs.py
@@ -1,8 +1,8 @@
 import requests
 import re
 
-# Prompt the user for the URL of the website to scrape
-url = input("Enter the URL of the website to scrape: ")
+# Set the URL of the website to scrape
+url = "https://www.google.com"
 
 # Open a file to write the results to
 with open("js_files.txt", "w") as output_file:
@@ -16,13 +16,15 @@ with open("js_files.txt", "w") as output_file:
   # Iterate through the list of JavaScript files
   for js_file in js_files:
     # Extract the URL of the JavaScript file
-    js_url = re.search(r"https?://[^']+", js_file).group()
+    pattern = r"https?://[^']+"
+    matches = re.finditer(pattern, js_file)
+    for match in matches:
+      js_url = match.group()
+      # Send a request to the JavaScript file and retrieve the content
+      js_response = requests.get(js_url)
+      js_content = js_response.text
 
-    # Send a request to the JavaScript file and retrieve the content
-    js_response = requests.get(js_url)
-    js_content = js_response.text
-
-    # Check if the "admin" context is present in the JavaScript file
-    if "admin" in js_content:
-      # Write the URL of the JavaScript file to the output file
-      output_file.write(js_url + "\n")
+      # Check if the "admin" context is present in the JavaScript file
+      if "admin" in js_content:
+        # Write the URL of the JavaScript file to the output file
+        output_file.write(js_url + "\n")
