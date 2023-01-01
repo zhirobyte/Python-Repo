a704cd94e3998871bf3f7fb2236978dbb2263108
zhirobyte <faizhanafi90@gmail.com>
2023-01-02 01:00:04 +0700

    adding scrap file update

--- Scrapjs.py
+++ Scrapjs.py
@@ -1,25 +1,28 @@
 import requests
 import re
 
-# Set the URL of the website to scrape
-url = "https://www.example.com"
+# Prompt the user for the URL of the website to scrape
+url = input("Enter the URL of the website to scrape: ")
 
-# Send a request to the website and retrieve the HTML
-response = requests.get(url)
-html = response.text
+# Open a file to write the results to
+with open("js_files.txt", "w") as output_file:
+  # Send a request to the website and retrieve the HTML
+  response = requests.get(url)
+  html = response.text
 
-# Use a regular expression to find all JavaScript files in the HTML
-js_files = re.findall(r"<script src=[^>]+", html)
+  # Use a regular expression to find all JavaScript files in the HTML
+  js_files = re.findall(r"<script src=[^>]+", html)
 
-# Iterate through the list of JavaScript files
-for js_file in js_files:
-  # Extract the URL of the JavaScript file
-  js_url = re.search(r"https?://[^']+", js_file).group()
+  # Iterate through the list of JavaScript files
+  for js_file in js_files:
+    # Extract the URL of the JavaScript file
+    js_url = re.search(r"https?://[^']+", js_file).group()
 
-  # Send a request to the JavaScript file and retrieve the content
-  js_response = requests.get(js_url)
-  js_content = js_response.text
+    # Send a request to the JavaScript file and retrieve the content
+    js_response = requests.get(js_url)
+    js_content = js_response.text
 
-  # Check if the "admin" context is present in the JavaScript file
-  if "admin" in js_content:
-    print("Found 'admin' in JavaScript file: " + js_url)
+    # Check if the "admin" context is present in the JavaScript file
+    if "admin" in js_content:
+      # Write the URL of the JavaScript file to the output file
+      output_file.write(js_url + "\n")
